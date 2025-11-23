ed0f572f194a4e530090985f76a030d98671595d
rospg <rpaniaguagarcia@stsaviour.org>
2025-11-22 19:43:55 -0500

    forgot to add something

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,37 +1,23 @@
 public class Player {
     
     public String name;
-   
     public int life = 3;
-
+    //every player haas a name and a set number of lives
 
     public Player (String name){
         this.name = name;
     }
-   
+   //creates a player
     public void losealife(){
         System.out.println (this.name + " lost a life!");
         life --; 
         System.out.println("Lives left: " + life );
     }
-
+    //creates a method to lose a life
     public static int rollD20(){
         int r = (int)(Math.random() * 20) + 1;
         Ascii.drawD20(r);
         return r;
     }
-
-    // public static int rollAdvantage(){
-    //     int first  = (int)(Math.random() * 20) + 1;
-    //     Ascii.drawD20(first);
-    //     int second =  (int)(Math.random() * 20) + 1;
-    //     Ascii.drawD20(second);
-    //     if (first > second){
-    //         return first;
-    //     }
-    //     else{
-    //         return second;
-    //     }
-    // }
-
+    //creates a method to roll a 20 sided die
 }
